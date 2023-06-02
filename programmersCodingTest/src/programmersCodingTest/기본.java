aba9f26871ec5ce730768ae45cf85e282e60229a
김보람 <dipporee@gmail.com>
2023-06-02 15:50:39 +0900

    a와b출력하기

---
+++ programmersCodingTest/src/programmersCodingTest/기본.java
@@ -0,0 +1,23 @@
+package programmersCodingTest;
+
+import java.util.Scanner;
+
+public class 기본 {
+
+	public static void main(String[] args) {
+		/*
+		 
+		 * 
+		 
+		 * 문제실행 : 
+		 * 제한사항 :
+		 * 입력 : 
+		 * 출력 : 
+		  	
+		 
+		 결론 : 
+		 	1. 
+		 */
+		
+	}
+}
