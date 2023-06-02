c0c016dd5b79d96c67ed52f11f2b72df794a20ed
김보람 <dipporee@gmail.com>
2023-06-02 14:44:38 +0900

    결론 첨가

--- programmersCodingTest/src/programmersCodingTest/codingTest/level0/문자열출력하기.java
+++ programmersCodingTest/src/programmersCodingTest/codingTest/level0/문자열출력하기.java
@@ -6,13 +6,19 @@ public class 문자열출력하기 {
 
 	public static void main(String[] args) {
 		/*
+		 
 		 * 문자열 출력하기
-		 * 
+		 
 		 * 문제실행 : 문자열 str이 주어질 때, str을 출력하는 코드를 작성해 보세요.
 		 * 제한사항 : 1 ≤ str의 길이 ≤ 1,000,000
 					str에는 공백이 없으며, 첫째 줄에 한 줄로만 주어집니다.
 		 * 입력 : HelloWorld!
 		 * 출력 : HelloWorld!			
+		 
+		 결론 : 
+		 	1. >= 와 <= 반대로 쓰면 안됨. 크거나 같다 작거나 같다 이런 순이어야 함.
+		 	2. System.out.print 외워야 해 
+		 
 		 */
 		
         Scanner sc = new Scanner(System.in);
