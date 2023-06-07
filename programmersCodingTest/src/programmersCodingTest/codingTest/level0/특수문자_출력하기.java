8934673472c268bdea63581f801e05fe2af03ff7
김보람 <dipporee@gmail.com>
2023-06-07 15:56:18 +0900

    특수문자 출력하기

---
+++ programmersCodingTest/src/programmersCodingTest/codingTest/level0/특수문자_출력하기.java
@@ -0,0 +1,23 @@
+package programmersCodingTest.codingTest.level0;
+
+public class 특수문자_출력하기 {
+
+	public static void main(String[] args) {
+		/*
+		 
+		 * 특수문자 출력하기
+		 
+		 * 문제실행 : 다음과 같이 출력하도록 코드를 작성해 주세요.
+		 * 제한사항 :
+		 * 입력 : 
+		 * 출력 : !@#$%^&*(\'"<>?:;
+		  	
+		 
+		 결론 : 
+		 	1. \를 한번 더 적어주어야 한다.  
+		 */
+		
+		System.out.print("!@#$%^&*(\\'\"<>?:;");
+		
+	}
+}
